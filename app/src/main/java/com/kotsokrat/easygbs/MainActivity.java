a0cc845aa26edf53dd29c3c93a668a2fa4943229
Stalenov <stalenov@gmail.com>
2016-07-20 23:19:37 +0300

    make class for start alertmanager

--- app/src/main/java/com/kotsokrat/easygbs/MainActivity.java
+++ app/src/main/java/com/kotsokrat/easygbs/MainActivity.java
@@ -55,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
                 new UpdateScreenData().execute(MainActivity.this);
             }
         });
+
+        //TODO перед обновлением из хттп вставлять данные из файла
     }
 
     @Override
--- app/src/main/java/com/kotsokrat/easygbs/OnSysStartReceiver.java
+++ app/src/main/java/com/kotsokrat/easygbs/OnSysStartReceiver.java
@@ -1,15 +1,34 @@
 package com.kotsokrat.easygbs;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class OnSysStartReceiver extends BroadcastReceiver {
     int delay;
+    final String tag = "myTag";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO написать запуск алертменеджера
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        delay = Integer.parseInt(prefs.getString("update_delay", "15"));
+
+        long notificationDelay = 60000 * delay;
+
+        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Log.d(tag, "Start alarm manager" + Long.toString(notificationDelay));
+        Intent notificatorIntent = new Intent(context ,GBSNotificator.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getBroadcast(context, 0, notificatorIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        am.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), notificationDelay, notificationPendingIntent);
+
+
 
     }
 }
